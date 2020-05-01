65083a9d0511578919a8976b9d1e138f4a09a8b9
Nikita Melnikov <nikmel2803@gmail.com>
2020-05-01 20:27:34 +0300

    fix scripts

--- mongorestore-accounts.sh
+++ mongorestore-accounts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose exec -T mongodb mongorestore --host mongodb --drop -d hawk /accounts-db-dump
+docker-compose exec -T mongodb mongorestore --host mongodb --drop -d hawk /dump/hawk_accounts
--- mongorestore-events.sh
+++ mongorestore-events.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose exec -T mongodb mongorestore --host mongodb --drop -d hawk_events /events-db-dump
+docker-compose exec -T mongodb mongorestore --host mongodb --drop -d hawk_events /dump/hawk_events
