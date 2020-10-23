89ffa8254dd4c86649bef0a15d898e4edea3eec3
ilyamore88 <themorozik@gmail.com>
2020-10-23 20:03:04 +0300

    fix mongo restore scripts on windows

--- mongorestore-events.sh
+++ mongorestore-events.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose exec -T mongodb mongorestore --host mongodb --drop -d hawk_events /dump/hawk_events
+docker-compose exec -T mongodb mongorestore --host mongodb --drop -d hawk_events ./dump/hawk_events
