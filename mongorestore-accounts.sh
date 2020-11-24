4156dbbdea707d16d1542c4df90f3f034a1468df
N0str <ntpcp@yandex.ru>
2020-11-24 13:31:50 +0300

    Add restore script with archive support Add proper mongodb mapping for /dump directory

--- mongorestore-accounts.sh
+++ mongorestore-accounts.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-docker-compose exec -T mongodb mongorestore --host mongodb --drop -d hawk /dump/hawk_accounts
+if [ $# -eq 0 ]
+then
+    result=$(docker-compose exec -T mongodb sh -c "ls -tr /dump/hawk_prod_mongodb-accountants_daily-full*.gz | head -1")
+    if [ -z "$result" ]
+    then
+        echo "No arguments supplied. Usage: ./mongorestore-accounts.sh /dump/backup.gz"
+        exit 1
+    fi
+else
+    result=$(docker-compose exec -T mongodb sh -c "ls -tr $1 | head -1")
+    if [ -z "$result" ]
+    then
+        echo "No such file. Check that you placed it into the mounted folder ./dump"
+        exit 1
+    fi
+fi
+
+docker-compose exec -T mongodb mongorestore --host mongodb --drop --archive=$result
