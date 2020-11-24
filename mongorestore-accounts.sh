b2f647317fb9a3e583df693a3c3a6732355aa0a4
N0str <ntpcp@yandex.ru>
2020-11-24 18:09:25 +0300

    Proper sort

--- mongorestore-accounts.sh
+++ mongorestore-accounts.sh
@@ -2,14 +2,14 @@
 
 if [ $# -eq 0 ]
 then
-    result=$(docker-compose exec -T mongodb sh -c "ls -tr /dump/hawk_prod_mongodb-accountants_daily-full*.gz | head -1")
+    result=$(docker-compose exec -T mongodb sh -c "ls -t /dump/hawk_prod_mongodb-accountants_daily-full*.gz | head -1")
     if [ -z "$result" ]
     then
         echo "No arguments supplied. Usage: ./mongorestore-accounts.sh /dump/backup.gz"
         exit 1
     fi
 else
-    result=$(docker-compose exec -T mongodb sh -c "ls -tr $1 | head -1")
+    result=$(docker-compose exec -T mongodb sh -c "ls -t $1 | head -1")
     if [ -z "$result" ]
     then
         echo "No such file. Check that you placed it into the mounted folder ./dump"
