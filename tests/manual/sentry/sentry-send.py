10baa89ba2e01bcbfe6a84b24d4e49a4b971ac80
n0str <nostr@MacBook-Pro-Aleksandr-5.local>
2024-12-05 14:55:20 +0300

    link sentry-support versions

--- tests/manual/sentry/sentry-send.py
+++ tests/manual/sentry/sentry-send.py
@@ -1,10 +1,11 @@
 import sentry_sdk
 import os
 
-HAWK_INTEGRATION_TOKEN = os.getenv("SENTRY_PROJECT_ID", "eyJpbnRlZ3JhdGlvbklkIjoiYzlmZTY1MzYtYjVkYi00NDNmLWI1MmEtMWQwNDY1OThkNDNiIiwic2VjcmV0IjoiNzAwZTAwYTQtM2E4Mi00NTQ3LTkzOGUtMTMzMGJhNzgwY2UwIn0=")
+HAWK_INTEGRATION_TOKEN = os.getenv("SENTRY_PROJECT_ID", "eyJpbnRlZ3JhdGlvbklkIjoiOWQ0MzUwMzgtMWQ3OS00NTlhLWJhMWUtZWQwMDVjYzI0NTM0Iiwic2VjcmV0IjoiZGU3ODZkMDAtNzE2Ni00ZTYzLWI5YzAtYTFjNjcyOWZlOGU5In0=")
 
 sentry_sdk.init(
     dsn=f"http://{HAWK_INTEGRATION_TOKEN}@localhost:3000/0",
     debug=True
 )
 division_by_zero = 1 / 0
+# raise Exception("This is a test exception")
